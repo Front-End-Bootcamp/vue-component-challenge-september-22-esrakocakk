09019f1d820e9d5585a8c2017f62bdb545dbf76e
Esra Koçak <esrakocak470@gmail.com>
2022-09-24 00:06:52 +0300

    revised

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <script setup>
 import BootcampLogo from "./assets/svg/BootcampLogo.vue"
 import HelloWorld from './components/HelloWorld.vue'
+
+//json dosyası import edildi.
+import DATA from '@/assets/data/data.json';
+import { onMounted, ref } from 'vue';
+
+
+const data = ref([]);
+
+//json dosyası içindeki veriler grupsal olarak ayrıldı.
+function getGroupNames(data){
+	const groupNames = data.map(person => person.group);
+	const uniqueNames = [...new Set(groupNames)];
+	return uniqueNames;
+}
+function filterByGroups(data){
+	const groups = data.reduce((currentData, person) => {
+		if(!currentData[person.group]){
+			currentData[person.group] = []
+		}
+
+		return currentData;
+	}, {});
+
+
+Object.keys(groups).forEach(group => {
+	const students = data.filter(person => person.group == group);
+	groups[group] = students;
+})
+
+return groups
+}
+
+const names = getGroupNames(DATA);
+console.log(names);
+
+const groups = filterByGroups(DATA);
+console.log(groups);
 </script>
 
 <template>
+
 	<div>
-		<BootcampLogo />
-		<br />
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
+		{{names}}
+		<br>
 	</div>
-	<HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
